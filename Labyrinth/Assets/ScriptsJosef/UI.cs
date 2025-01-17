f14db556b5da78209b57b57623dcfac9fc1e9dbe
Josef <josef.aref@elev.ga.lbs.se>
2025-01-17 12:06:42 +0100

    Påbörjan av framtida delar

--- Labyrinth/Assets/ScriptsJosef/UI.cs
+++ Labyrinth/Assets/ScriptsJosef/UI.cs
@@ -5,11 +5,27 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    Image Img; 
+    Image Img;
+    public GameObject objectID01;
+    public GameObject objectID02;
+    public GameObject objectID03;
+
+    public GameObject objectID01_2;
+    public GameObject objectID02_2;
+    public GameObject objectID03_2;
+
+    public GameObject objectID01_3;
+    public GameObject objectID02_3;
+    public GameObject objectID03_3;
+
+    public GameObject Item1;
+    public GameObject Item2;
+    public GameObject Item3;
+
     // Start is called before the first frame update
     void Start()
     {
-        Img = GetComponent<Image>();
+
     }
 
     // Update is called once per frame
